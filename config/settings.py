cfc9fc231132197e587f4bdca7e8921308d697e6
Axlidin <akhlidinshermatov00@gmail.com>
2025-05-26 10:36:58 +0500

    .env sozlandi

--- config/settings.py
+++ config/settings.py
@@ -1,5 +1,7 @@
 from pathlib import Path
-
+import os
+import environ
+env = environ.Env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -7,14 +9,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@44nj-9@mla=9xx*jcf@m8z1yxe8eo%x16zi@-e=c0elr(u6l0'
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))  # faqat lokalda o‘qiladi
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ["*"]
-
+DEBUG = env.bool("DEBUG", default=False)
+SECRET_KEY = env("SECRET_KEY", default="fallback-key")
 # Application definition
 
 INSTALLED_APPS = [
